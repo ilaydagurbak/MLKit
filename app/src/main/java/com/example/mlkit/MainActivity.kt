3a1757491d696ff0785edaf7f37c10ef69d1b382
ilaydagurbak <ilaydagurbak@gmail.com>
2020-04-29 14:02:36 +0200

    Label image from gallery

--- app/src/main/java/com/example/mlkit/MainActivity.kt
+++ app/src/main/java/com/example/mlkit/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.example.mlkit
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private val pickPhotoRequestCode = 150
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         val fab: View = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
+        fab.setOnClickListener {
             pickImage()
         }
 
@@ -24,8 +30,49 @@ class MainActivity : AppCompatActivity() {
     private fun pickImage() {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
-        startActivityForResult(intent, pickPhotoRequestCode)
+        startActivityForResult(intent, Companion.PHOTO_REQUEST_CODE)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                Companion.PHOTO_REQUEST_CODE -> {
+                    val bitmap = getImageFromData(data)
+                    bitmap?.apply {
+                        photoIV.setImageBitmap(this)
+                        processImageTagging(bitmap)
+                    }
+                }
+            }
+        }
+        super.onActivityResult(
+            requestCode, resultCode,
+            data
+        )
+    }
+
+    private fun getImageFromData(data: Intent?): Bitmap? {
+        val selectedImage = data?.data
+        return MediaStore.Images.Media.getBitmap(
+            this.contentResolver,
+            selectedImage
+        )
+    }
+
+    private fun processImageTagging(bitmap: Bitmap) {
+        val visionImg = FirebaseVisionImage.fromBitmap(bitmap)
+        FirebaseVision.getInstance().onDeviceImageLabeler.processImage(visionImg)
+            .addOnSuccessListener { tags ->
+                labelsTV.text = tags.sortedByDescending { it.confidence }
+                    .joinToString(" ") { it.text }
+            }
+            .addOnFailureListener { ex ->
+                Log.wtf("LAB", ex)
+            }
+    }
+
+    companion object {
+        private const val PHOTO_REQUEST_CODE = 180
+    }
 
 }
